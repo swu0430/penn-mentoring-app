47aaf2050a9735a1bfb26ce645c8fcc579ed88fd
sang29 <sangik.han29@gmail.com>
2021-01-17 17:01:35 -0500

    profiles page updated

--- src/components/profiles.js
+++ src/components/profiles.js
@@ -12,6 +12,7 @@ class Profiles extends React.Component {
       currentUser: null
     };
   };
+
   // firestore() = useFirestore();
   // userCollection() = firestore.collection('users');
   // firebase() = useFirebaseApp();
@@ -21,33 +22,81 @@ class Profiles extends React.Component {
   }
 
   async setProfiles() {
-    // const admin = require('firebase-admin');
-    // admin.initializeApp();
     const db = firebase.app().firestore();
     const snapshot = await db.collection('users').get();
+    var users = [];
+
     snapshot.forEach((doc) => {
-      console.log(doc.id, '=>', doc.data());
+      // var current
+      // return <div> {doc.data()} </div>;
+      users.push(doc.data())
+      // console.log(doc.id, '=>', doc.data());
+
     });
-
-    // console.log(snapshot);
     // this.setState({profiles: snapshot})
-
+    //
+    // // console.log(snapshot);
+    // // console.log(users);
+    this.setState({profiles: users})
+    console.log(this.state.profiles);
   }
 
 
   async componentDidMount() {
     await this.setCurrentUser();
     await this.setProfiles();
-    console.log(this.state.currentUser)
+    // console.log(this.state.currentUser)
   }
 
 
   render() {
+    // const profileCollection = this.state.profile.map((profile, index) => {
+    //   return (
+    //     <div>
+    //       <p> Penn Affiliation: {profile.pennGroup}</p>
+    //     </div>
+    //   )
+    // })
+
     return (
       <div>
-        {this.state.profiles}
+        <h3> Find profiles that you'd like to connect with! </h3>
+        <br/>
+        {
+          this.state.profiles.map((profile) =>
+          <div className="ui segment">
+            <b> Penn Affiliation </b>
+            <div>{profile.pennGroup}</div>
+            <br/>
+
+            <b>Job Experience</b>
+            {profile.jobs.map((job) => <li> {job.job} @ {job.org} </li>)}
+            <br/>
+
+            <b>Career Interests</b>
+            {profile.jobInterests.map((job) => <li> Interested in: {job.job} @ {job.org} </li>)}
+            <br/>
+
+            <button className="ui button"> Contact </button>
+          </div>
+
+        )}
+
       </div>
-    );
+    )
+
+
+    // return (
+    //   <div>
+  //   {this.state.profiles.map((profile, index) => (
+  //     <div className="ui card">
+  //       <div className="content">
+  //         Penn Affiliation: {profile.pennGroup} <br/>
+  //         Work Experience <br/>
+  //       </div>
+  //   </div>)
+  // }
+  // </div>;
   }
 }
 
